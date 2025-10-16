874d3814839e716b10b7704e88c7ae2982989cac
chethanbtn <31713087+Deviation-From-The-Norm@users.noreply.github.com>
2025-10-16 12:07:57 -0500

    update things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BigBlueAutoDontUse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BigBlueAutoDontUse.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous(name="Blue Big Auto - kitbot", group="Robot")
-public class BigBlueAuto extends AutoTemplate {
+public class BigBlueAutoDontUse extends AutoTemplate {
     public void init() {
         setupAuto();
     }
