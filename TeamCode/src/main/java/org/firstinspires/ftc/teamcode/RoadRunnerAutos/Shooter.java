e50b70fdf7128192f42fa0ca54313a4d1924bb55
DaBob77 <DFN6093@gmail.com>
2025-11-26 17:12:50 -0600

    Beginning actions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerAutos/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerAutos/Shooter.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.RoadRunnerAutos;
 
-import android.app.Notification;
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 
 import androidx.annotation.NonNull;
 
@@ -9,36 +9,50 @@ import com.acmerobotics.roadrunner.*;
 import com.acmerobotics.roadrunner.ftc.*;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
 public class Shooter {
     private DcMotorEx motor;
+    private double rumbleStrength = 1;
 
     public Shooter(HardwareMap hardwareMap) {
         motor = hardwareMap.get(DcMotorEx.class, "launcher");
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor.setDirection(DcMotor.Direction.REVERSE);
 
+        motor.setZeroPowerBehavior(BRAKE);
+
         motor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, 0, 10));
     }
-    public Action moveToPoint(double x, double y) {
+    public Action spinUp(int targetSpeed) {
         return new Action() {
             private boolean initialized = false;
 
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
-                    motor.setPower(0.8);
+                    motor.setVelocity(targetSpeed);
                     initialized = true;
                 }
+
+                double vel = motor.getVelocity();
+                packet.put("Shooter Velocity", vel);
+                return vel > targetSpeed;
+
             }
+        };
 
-        }
-
+    }
+    public Action rumble(Gamepad gamepad1) {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                gamepad1.rumble(rumbleStrength, rumbleStrength, -1);
+                return false;
+            }
+        };
     }
 
 }
